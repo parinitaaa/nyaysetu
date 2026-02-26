cff9da8ce56292a14e35f6d8df8554a852350158
mrudulavinay30 <mrudulavinay30@users.noreply.github.com>
2026-02-26 11:11:08 +0530

    deleted unneccessary files

--- client/src/services/rightsApi.js
+++
@@ -1,9 +0,0 @@
-const BASE_URL = "http://localhost:8000";
-
-export async function fetchRightsIndex() {
-  const response = await fetch(`${BASE_URL}/rights/categories`);
-  if (!response.ok) {
-    throw new Error("Failed to fetch rights index");
-  }
-  return response.json();
-}
